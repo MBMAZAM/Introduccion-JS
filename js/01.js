6e84a9decc2244e3183bd4d3fa2eaf9fe10facbe
MBMAZAM <maza_1506@hotmail.com>
2023-03-31 02:09:45 -0500

    Viendo Variables

---
+++ js/01.js
@@ -0,0 +1,20 @@
+// Variables
+
+var producto = 'Audifonos Gamer';  // Iniciar variable y asignar valor.
+
+var disponible; // Iniciamos la variable sin valor.
+
+//producto = true; // Reasignando el valor de la variable.
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+
+
+var _hola;
+
+
+console.log(producto) //Imprime el mensaje de la consola
