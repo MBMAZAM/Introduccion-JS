c55a0b9541f9448cffc6f529bf64e349bb49d9a7
MBMAZAM <maza_1506@hotmail.com>
2023-04-03 17:31:39 -0500

    Avanzado

---
+++ js/13.destructuring.js
@@ -0,0 +1,19 @@
+// Objeto
+const producto = {
+    nombreProducto : "Monitor 20 Pulgadas",
+    precio : 300,
+    disponible : true
+}
+
+
+// // Forma Anterior
+// const precioProducto = producto.precio;
+// const nombreProducto = producto.nombreProducto;
+
+// Destructuring - Arreglo o Objeto
+
+const {precio, nombreProducto} = producto;
+
+
+console.log(precio);    
+console.log(nombreProducto);  
