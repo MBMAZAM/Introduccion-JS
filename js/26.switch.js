2f295ce85e5c91d61932e7af80f69ef8b151dca6
MBMAZAM <maza_1506@hotmail.com>
2023-04-29 11:11:19 -0500

    Avanzando

--- js/26.switch.js
+++ js/26.switch.js
@@ -1,1 +1,21 @@
-const metodoPago = 'tarjeta';
+const metodoPago = 'cheque';
+
+switch (metodoPago) {
+    case 'tarjeta':
+        console.log('Pagaste con tarjeta');
+        break;
+    case 'cheque':
+        console.log('El usuario va pagar con cheque');
+        break;    
+    default:
+        console.log('Aún no has pagado');
+        break;
+}
+
+
+
+
+
+
+
+
---
+++ js/27.forLoop.js
@@ -0,0 +1,12 @@
+// For Loop
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+
+
+// While Loop
+
+
+// Do While Loop
