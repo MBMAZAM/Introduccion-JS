dbbdeeb2cac9d01c5d2b029f51b744327ea9e452
MBMAZAM <maza_1506@hotmail.com>
2023-05-06 14:56:33 -0500

    Avanzando

--- js/30.objConstructor-Lineal.js
+++ js/30.objConstructor-Lineal.js
@@ -18,16 +18,36 @@ function Producto(nombre, precio, modelo) { //Parametros formales //Parametros
     this.modelo = modelo;
 }
 
+// Crear funciones que solo se utilizan en un objeto en especifico
+Producto.prototype.formatearProducto = function() {
+    return `El producto ${this.nombre} tiene un precio de : $ ${this.precio}`;
+}
+
 //Instancia ↓
 const producto2 = new Producto('Monitor Curvo de 49"', 800); //Parametros reales //Argumentos 
 const producto3 = new Producto('Laptop"', 300); 
 const producto4 = new Producto('Celular"', 2800, "LG"); 
 
+
 console.log(producto2);
 console.log(producto3);
 console.log(producto4);
 
+console.log(formatearProducto(producto2));
 
-function formatearProducto(params) {
-    
+
+function Cliente(nombre, apellido) { 
+    this.nombre = nombre;
+    this.apellido = apellido;
 }
+
+function formatearProducto(producto) {
+    return `El producto ${producto.nombre} tiene un precio de : $ ${producto.precio}`;
+}
+
+const cliente = new Cliente('Mauricio', 'Maza');
+
+
+console.log(cliente);
+
+console.log(formatearProducto(cliente));
