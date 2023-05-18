9b7374cc0e0387db18ac5db51e0df23e482f1df2
MBMAZAM <maza_1506@hotmail.com>
2023-05-18 01:01:01 -0500

    Avanzando

--- js/32.clasesPOO.js
+++ js/32.clasesPOO.js
@@ -2,7 +2,23 @@
 // CLASES
 
 class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 
+    formatearProducto() {
+        return `El Producto ${this.nombre} tiene un precio de $ ${this.precio}`;
+    }
+
+    EnviarPrecio () {
+
+    }
 }
 
-const producto = new Producto();
+const producto2 = new Producto('Monitor Curvo de 49"', 800); 
+const producto3 = new Producto('Laptop"', 300); 
+
+console.log(producto2);
+console.log(producto2.formatearProducto());
+console.log(producto3);
