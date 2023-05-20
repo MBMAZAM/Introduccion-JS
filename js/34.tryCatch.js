d72fc7e5b9cdf0c42df9f48ec474c1fe8363a250
MBMAZAM <maza_1506@hotmail.com>
2023-05-20 03:32:03 -0500

    Avanzando

--- js/34.tryCatch.js
+++ js/34.tryCatch.js
@@ -5,11 +5,10 @@ const numero3 = 30;
 
 console.log(numero1);
 
-
 try {
-    
+    console.log(numero2);
 } catch (error) {
-    
+    console.log(error);
 }
 
 console.log(numero3);
---
+++ js/35.promises.js
@@ -0,0 +1,4 @@
+
+const usuarioAutentico = new Promise(function(){
+    
+})
